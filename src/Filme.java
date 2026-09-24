bafaa6daa397906d5ad4d90e0f135b48abdb2276
Nicole Campos Alves <nicolecamposmedici@gmail.com>
2026-09-24 11:09:41 -0300

    Update Filme.java

--- src/Filme.java
+++ src/Filme.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Filme {
+        String nome;
+        int anoDeLancamento;
+        double duracaoEmMinutos;
+        boolean incluidoNoPlano;
+        double somaDasAvaliacoes;
+        double totalDasAvaliacoes;
+
+        void exibeFichaDoFilme() {
+                System.out.println("Nome do filme: " + nome);
+                System.out.println("Ano de lancamento: " + anoDeLancamento);
+        }
+
+        void avalia(double nota) {
+                somaDasAvaliacoes += nota;
+                totalDasAvaliacoes++;
+        }
+
+        double pegaMedia() {return somaDasAvaliacoes/totalDasAvaliacoes;}
 }
